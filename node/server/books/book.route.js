1a81999a1b1156996cb2984bd9fc975385194039
Nguyễn Duy Hiếu <hidxhidxhid@gmail.com>
2019-11-30 11:57:26 +0700

    sex

--- node/server/books/book.route.js
+++ node/server/books/book.route.js
@@ -2,6 +2,11 @@ const router = require('express').Router()
 const Book = require('../models/book.schema')
 const BookMatrix = require('../models/book.matrix.schema')
 
+router.get('/', async (req, res) => {
+    const books = await Book.find({})
+    res.send(books)
+})
+
 router.get('/:id', async (req, res) => {
     const book = await Book.findOne({ book_id: req.params.id })
     res.send(book)
